c93f3c731980923948fa52be569254f3fabc991d
Elizabeth Vega M <eb.vega.m@gmail.com>
2018-10-25 09:10:20 -0300

    trabajando con datos dashboard

--- public/src/JS/dashboard.js
+++ public/src/JS/dashboard.js
@@ -1,3 +1,309 @@
+const data = [{
+  "name": "José Tolosa",
+  "profile": "Front-End Developer",
+  "experience": "3-4 años",
+  "availability": "Inmediata",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "700-800mil",
+  "score": 725,
+  "skills": {
+    "HTML": 5,
+    "CSS": 4,
+    "JavaScript": 4,
+    "React": 0,
+    "Angular": 2,
+    "Bootstrap": 4,
+    "NodeJS": 1,
+    "Git": 2
+  },
+  "soft": {
+    "Organización": 3, 
+    "Responsabilidad": 4, 
+    "Innovación": 2, 
+    "Liderazgo":3, 
+    "Trabajo en Equipo": 3, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 3
+  }
+},
+{
+  "name": "Romina Muñoz",
+  "profile": "UX Designer",
+  "experience": "0-1 años",
+  "availability": "Inmediata",
+  "languages": ["Español"],
+  "expectedSalary": "500-600mil",
+  "score": 650,
+  "skills": {
+    "Prototipado": 4,
+    "Testing": 4,
+    "Diseño UI": 3,
+    "Reaearch": 1,
+    "Comunicación Visual": 3
+  },
+  "soft": {
+    "Organización": 5, 
+    "Responsabilidad": 4, 
+    "Innovación": 4, 
+    "Liderazgo":1, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 3, 
+    "Autoaprendizaje": 4
+  }
+},
+{  
+  "name": "Alejandra Ibarra",
+  "profile": "UX Designer",
+  "experience": "0-1 años",
+  "availability": "Inmediata",
+  "languages": ["Español"],
+  "expectedSalary": "300-400mil",
+  "score": 480,
+  "skills": {
+    "Prototipado": 4,
+    "Testing": 4,
+    "Diseño UI": 3,
+    "Reaearch": 1,
+    "Comunicación Visual": 3
+  },
+  "soft": {
+    "Organización": 5, 
+    "Responsabilidad": 4, 
+    "Innovación": 4, 
+    "Liderazgo":1, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 3, 
+    "Autoaprendizaje": 4
+  }
+},
+{
+  "name": "Natalia Martínez",
+  "profile": "Front-End Developer",
+  "experience": "4 años +",
+  "availability": "1-2semanas",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "800mil +",
+  "score": 830,
+  "skills": {
+    "HTML": 5,
+    "CSS": 4,
+    "JavaScript": 4,
+    "React": 4,
+    "Angular": 3,
+    "Bootstrap": 4,
+    "NodeJS": 4,
+    "Git": 4
+  },
+  "soft": {
+    "Organización": 3, 
+    "Responsabilidad": 4, 
+    "Innovación": 2, 
+    "Liderazgo":3, 
+    "Trabajo en Equipo": 3, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 5
+  }
+},
+{
+  "name": "Diana Spencer",
+  "profile": "Front-End Developer",
+  "experience": "0-1 años",
+  "availability": "1-2semanas",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "400-500mil",
+  "score": 625,
+  "skills": {
+    "HTML": 4,
+    "CSS": 4,
+    "JavaScript": 4,
+    "React": 1,
+    "Angular": 1,
+    "Bootstrap": 3,
+    "NodeJS": 4,
+    "Git": 4
+  },
+  "soft": {
+    "Organización": 3, 
+    "Responsabilidad": 4, 
+    "Innovación": 2, 
+    "Liderazgo":3, 
+    "Trabajo en Equipo": 3, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 4
+  }
+},
+{
+  "name": "Ricardo Seguel",
+  "profile": "Front-End Developer",
+  "experience": "1-2 años +",
+  "availability": "1-2semanas",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "700-800mil",
+  "score": 830,
+  "skills": {
+    "HTML": 5,
+    "CSS": 4,
+    "JavaScript": 4,
+    "React": 4,
+    "Angular": 3,
+    "Bootstrap": 4,
+    "NodeJS": 4,
+    "Git": 4
+  },
+  "soft": {
+    "Organización": 3, 
+    "Responsabilidad": 4, 
+    "Innovación": 2, 
+    "Liderazgo":3, 
+    "Trabajo en Equipo": 3, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 5
+  }
+},
+{
+  "name": "Macarena Painemal",
+  "profile": "UX Designer",
+  "experience": "0-1 años",
+  "availability": "Inmediata",
+  "languages": ["Español", "Inglés", "Alemán"],
+  "expectedSalary": "700-800mil",
+  "score": 740,
+  "skills": {
+    "Prototipado": 4,
+    "Testing": 4,
+    "Diseño UI": 3,
+    "Reaearch": 1,
+    "Comunicación Visual": 3
+  },
+  "soft": {
+    "Organización": 5, 
+    "Responsabilidad": 4, 
+    "Innovación": 4, 
+    "Liderazgo":1, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 3, 
+    "Autoaprendizaje": 4
+  }
+},
+{
+  "name": "Ximena Vega",
+  "profile": "UX Designer",
+  "experience": "0-1 años",
+  "availability": "Inmediata",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "400-500mil",
+  "score": 635,
+  "skills": {
+    "Prototipado": 4,
+    "Testing": 4,
+    "Diseño UI": 3,
+    "Reaearch": 1,
+    "Comunicación Visual": 3
+  },
+  "soft": {
+    "Organización": 5, 
+    "Responsabilidad": 4, 
+    "Innovación": 4, 
+    "Liderazgo":1, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 3, 
+    "Autoaprendizaje": 4
+  }
+},
+{
+  "name": "Sabrina Soto",
+  "profile": "UX Designer",
+  "experience": "0-1 años",
+  "availability": "Inmediata",
+  "languages": ["Español"],
+  "expectedSalary": "400-500mil",
+  "score": 720,
+  "skills": {
+    "Prototipado": 4,
+    "Testing": 4,
+    "Diseño UI": 3,
+    "Reaearch": 4,
+    "Comunicación Visual": 3
+  },
+  "soft": {
+    "Organización": 5, 
+    "Responsabilidad": 4, 
+    "Innovación": 4, 
+    "Liderazgo":1, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 3, 
+    "Autoaprendizaje": 4
+  }
+},
+{
+  "name": "Catalina Méndez",
+  "profile": "Front-End Developer",
+  "experience": "3-4 años",
+  "availability": "Inmediata",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "800mil +",
+  "score": 910,
+  "skills": {
+    "HTML": 5,
+    "CSS": 4,
+    "JavaScript": 5,
+    "React": 4,
+    "Angular": 4,
+    "Bootstrap": 4,
+    "NodeJS": 3,
+    "Git": 4
+  },
+  "soft": {
+    "Organización": 4, 
+    "Responsabilidad": 4, 
+    "Innovación": 5, 
+    "Liderazgo":4, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 5
+  }
+},
+{
+  "name": "Maripaz Acuña",
+  "profile": "Front-End Developer",
+  "experience": "1-2 años",
+  "availability": "Inmediata",
+  "languages": ["Español", "Inglés"],
+  "expectedSalary": "700-800mil",
+  "score": 680,
+  "skills": {
+    "HTML": 5,
+    "CSS": 4,
+    "JavaScript": 5,
+    "React": 0,
+    "Angular": 3,
+    "Bootstrap": 4,
+    "NodeJS": 2,
+    "Git": 4
+  },
+  "soft": {
+    "Organización": 4, 
+    "Responsabilidad": 4, 
+    "Innovación": 5, 
+    "Liderazgo":4, 
+    "Trabajo en Equipo": 4, 
+    "Análisis": 4, 
+    "Autoaprendizaje": 5
+  }
+}
+]
+
+window.onload = () => {
+  data.forEach(element => {
+    let name = element.name;
+    rank.innerHTML += `<div class="ranked">
+        <a>${name.match(/\b(\w)/g).join('')}</a>
+      </div>`
+  })
+}
+
+
+
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
