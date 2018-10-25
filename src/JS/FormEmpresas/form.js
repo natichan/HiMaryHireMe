b7d3f45b7a93f242275980686dd335507cd179ad
Natalia <nataliamh.88@gmail.com>
2018-10-24 23:13:02 -0300

    form tomando forma

---
+++ src/JS/FormEmpresas/form.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('goQuestions').addEventListener('click', (e) => {
+    document.getElementById('welcome').style.display = "none";
+    document.getElementById('questions').style.display = "block";
+})
